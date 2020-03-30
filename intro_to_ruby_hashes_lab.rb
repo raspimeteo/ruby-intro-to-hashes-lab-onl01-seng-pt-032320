e365189fdf277d85813ce05cc992e55538af8cba
John Zonneveld <jjczon@gmail.com>
2020-03-30 22:47:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,14 +18,8 @@ def monopoly_with_second_tier
     :railroads => 
     {
       :pieces => 4,
-      :names => 
-      {
-
-      },
-      :rent_in_dollars => 
-      {
-
-      }
+      :names => {},
+      :rent_in_dollars => {}
     }
     
   }
@@ -36,16 +30,16 @@ def monopoly_with_third_tier
   monopoly = {:railroads => {
     :pieces => 4,
     :names => {
-              :reading_railroad => {},
+        :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline => {}
     },
     :rent_in_dollars => {
-        :one_place_owned => 25,
-        :two_places_owned => 50,
-        :three_places_owned => 100,
-        :four_places_owned => 200
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
     }
   }}
 end
