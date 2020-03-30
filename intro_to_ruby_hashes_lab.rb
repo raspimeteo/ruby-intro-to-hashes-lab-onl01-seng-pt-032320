1b08565c08c33421f596324b5cf67b45eef49770
John Zonneveld <jjczon@gmail.com>
2020-03-30 22:52:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,7 +50,7 @@ monopoly = {:railroads => {
     :pieces => 4,
     :names => {
         :reading_railroad => {
-          :mortgage_value => "$100"
+          'mortgage_value' => "$100"
         },
         :pennsylvania_railroad => {
           :mortgage_value => "$200"
